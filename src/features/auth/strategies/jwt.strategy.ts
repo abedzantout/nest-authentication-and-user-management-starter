a11d0feb1b32cd8bffe4f69a0f5f16bcb916e7a5
Abed Zantout <abed@techhive.io>
2020-06-19 14:09:35 +0300

    feat(jwt): added jwt verification

--- src/features/auth/strategies/jwt.strategy.ts
+++ src/features/auth/strategies/jwt.strategy.ts
@@ -1,8 +1,26 @@
 import { PassportStrategy } from '@nestjs/passport';
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ExtractJwt, Strategy } from 'passport-jwt';
+import { HttpException } from '@nestjs/common/exceptions/http.exception';
+
+import { ConfigService } from '../../../core/config/config.service';
+import { JwtService } from '../services/jwt.service';
+import { User } from '../../../shared/users/schemas/user.schema';
+import { LoginCredentials } from '../models/credentials.interface';
+
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private readonly configService: ConfigService, private readonly jwtService: JwtService) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      passReqToCallback: true,
+      secretOrKey: configService.JWT.Key,
+    });
+  }
 
+  public async validate(payload: any, request: LoginCredentials): Promise<User | HttpException> {
+    const user = await this.jwtService.validateUser(request);
+    return user || new UnauthorizedException();
+  }
 }

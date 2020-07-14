e5ca9867a0d02ad7d94c404196fba527706fc2ef
Abed Zantout <abed@techhive.io>
2020-07-14 11:56:26 +0300

    test(users): added connection test

--- src/shared/users/services/users.service.spec.ts
+++ src/shared/users/services/users.service.spec.ts
@@ -1,8 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersService } from './users.service';
+import { MongoClient } from 'mongodb';
 
 describe('UsersService', () => {
   let service: UsersService;
+  let connection;
+  let db;
+
+  beforeAll(async () => {
+    connection = await MongoClient.connect('', {
+      useNewUrlParser: true,
+    });
+    db = await connection.db('nestjs_test');
+    console.log(db);
+  });
+
+  afterAll(async () => {
+    await connection.close();
+    await db.close();
+  });
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({

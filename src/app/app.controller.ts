39f60f4878b57c4de97fdfa402b9043c2eb28cb8
Abed Zantout <abed@techhive.io>
2020-07-22 12:29:06 +0300

    feat(app): add custom JWT auth guard

--- src/app/app.controller.ts
+++ src/app/app.controller.ts
@@ -1,13 +1,13 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { AppService } from './app.service';
-import { AuthGuard } from '@nestjs/passport';
+import { JwtAuthGuard } from '../core/guards/jwt-auth.guard';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
   @Get('restricted')
-  @UseGuards(AuthGuard('jwt'))
+  @UseGuards(JwtAuthGuard)
   getHello(): string {
     return this.appService.getHello();
   }
